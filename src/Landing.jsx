4318d3c0dfee19113f77d116e88a7bb84ba9f3e2
toms-source <gerwincristobal11@gmail.com>
2023-03-08 13:35:35 +0800

    update

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Typography, Divider } from "@mui/material";
 import Appbar from "./Components/Landing/Appbar";
 import Cards from "./Components/Landing/Cards";
@@ -8,45 +8,22 @@ import Info from "./Components/Landing/Info";
 import TutorialVid from "./Components/Landing/TutorialVid";
 import Developers from "./Components/Landing/Developers";
 import Footer from "./Components/Landing/Footer";
-
-import HashLoader from "react-spinners/HashLoader";
-
 const Landing = () => {
-  const [load, setLoad] = useState(false);
-  useEffect(() => {
-    setLoad(true);
-    setTimeout(() => {
-      setLoad(false);
-    }, 4100);
-  }, []);
   return (
     <>
-      {load ? (
-        <HashLoader
-          color={"#880000"}
-          loading={load}
-          size={80}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-          style={{ display: "grid", placeItems: "center" }}
-        />
-      ) : (
-        <>
-          <Appbar />
-          <ImgCarousel />
-          <Header />
-          <Cards />
-          <Info />
-          <TutorialVid />
-          <Developers />
-          <Divider>
-            <Typography color="#939393" textAlign="center">
-              © PUPSMB E-WAIT
-            </Typography>
-          </Divider>
-          <Footer />
-        </>
-      )}
+      <Appbar />
+      <ImgCarousel />
+      <Header />
+      <Cards />
+      <Info />
+      <TutorialVid />
+      <Developers />
+      <Divider>
+        <Typography color="#939393" textAlign="center">
+          © PUPSMB E-WAIT
+        </Typography>
+      </Divider>
+      <Footer />
     </>
   );
 };
